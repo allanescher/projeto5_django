780b5e24ad2fc283fcc898e89d21446b9af039df
allanescher <allanbescher@hotmail.com>
2022-02-21 21:56:28 -0300

    ajustes para pagina

--- projeto5/settings.py
+++ projeto5/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projeto5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'projeto5/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
